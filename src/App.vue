1fcce753ae553f954ec97beb63158dfd880b1686
Guillaume <garnier.guillaume37550@gmail.com>
2025-09-09 12:15:12 +0200

    premier commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+let string  = "Les mémoires de Steven Seagal"
+let boolean = false
+let array = ["du texte", 99]
+let object = {"name": "COOL", "tel": "0605030330"}
+let hello ="Hello World"
+
+</script>
+
+<template>
+  <div class="flex flex-col">
+    {{string }}
+    {{ boolean }}
+    {{ array }}
+    {{ array[0] }}
+    {{ object }}
+    {{ object.name }}
+    {{ object.name }}
+    {{ object.tel }}
+    {{ object.tel }}
+    {{ hello }}
+  </div>
+
+
+</template>
+
+<style scoped>
+</style>
